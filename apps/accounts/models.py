4684e52174964fc6a190e2578d3a40e864562296
Bruno Vidal <brunovidal27.19@gmail.com>
2025-07-25 17:48:28 -0300

    Adiciona modelo UserType com opções para diferentes tipos de usuários

--- apps/accounts/models.py
+++ apps/accounts/models.py
@@ -1,3 +1,9 @@
 from django.db import models
+sum
 
 # Create your models here.
+class UserType(models.TextChoices):
+    MANAGER = 'MANAGER', 'Supervisor'
+    CUSTOMER = 'CUSTOMER', 'Cliente'
+    DELIVERY_PERSON = 'DELIVERY_PERSON', 'Motoboy'
+
