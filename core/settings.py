03641c3cd0aa443c50923ead79431cf622d4d075
bruno vidal <brunovidal27.19@gmail.com>
2025-07-26 00:37:08 -0300

    Refactor code structure for improved readability and maintainability

--- core/settings.py
+++ core/settings.py
@@ -28,7 +28,9 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'apps.customers',
     'apps.orders',
-    'apps.accounts'
+    'apps.accounts',
+    'widget_tweaks',
+    'crispy_forms',
 ]
 
 MIDDLEWARE = [
@@ -94,7 +96,7 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/5.2/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'pt-BR'
 
 TIME_ZONE = 'UTC'
 
