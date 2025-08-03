27f0d2236fdcfbd58803ca53be93790c00e32e06
bruno vidal <brunovidal27.19@gmail.com>
2025-08-02 23:53:09 -0300

    feat: Atualizar tipo de pedido para incluir 'MotoBoy' e ajustar o valor padrão

--- apps/orders/models.py
+++ apps/orders/models.py
@@ -8,7 +8,7 @@ from django.utils import timezone
 
 class TypeOrder(models.TextChoices):
     MAIL = "MAIL", "Correios"
-    WHATSAPP = "WHATSAPP", "Whatsapp"
+    DELIVERY = "DELIVERY", "MotoBoy"
 
 
 class DeliveryStatus(models.TextChoices):
@@ -44,7 +44,7 @@ class Order(models.Model):  # TABELA PEDIDO
         default=DeliveryStatus.PENDING,
         verbose_name='Status Entrega' # Adicionado verbose_name aqui
     )
-    type_order = models.CharField(max_length=20, choices=TypeOrder.choices, default=TypeOrder.WHATSAPP, verbose_name='Tipo de Pedido')
+    type_order = models.CharField(max_length=20, choices=TypeOrder.choices, default=TypeOrder.DELIVERY, verbose_name='Tipo de Pedido')
     total_amount = models.FloatField(verbose_name='Valor Total') # Adicionado verbose_name
     created_at = models.DateTimeField(auto_now_add=True, verbose_name='Criado Em') # Adicionado verbose_name
     updated_at = models.DateTimeField(auto_now=True, verbose_name='Atualizado Em') # Adicionado verbose_name
